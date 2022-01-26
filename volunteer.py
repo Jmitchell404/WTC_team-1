c9b91744a18dade37ab82085385817231524dbb4
jmitchel020 <jmitchel020@student.wethinkcode.co.za>
2022-01-26 11:10:57 +0200

    test

---
+++ volunteer.py
@@ -0,0 +1,9 @@
+def volunteer_login():
+    input("are you a volunteer or a student?")
+    
+    if input == "volunteer":
+        print("would you like to see the available booking slots?")
+    elif input == "student":
+        print("these are the slots you can book")
+
+volunteer_login()
