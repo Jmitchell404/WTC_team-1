cb7ef6eca7d53f0513ee0e348fbfee95e0656c01
wtc <wtc@pop-os.localdomain>
2022-01-26 15:30:16 +0200

    Rylands test

--- volunteer.py
+++ volunteer.py
@@ -1,6 +1,6 @@
 def volunteer_login():
     input("are you a volunteer or a student?")
-    
+    #Ryland testing
     if input == "volunteer":
         print("would you like to see the available booking slots?")
     elif input == "student":
